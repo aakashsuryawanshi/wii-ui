04a7dbe9e535fd1e32e4bb706b7e4e72db2fd575
Akash Suryawanshi <s.akashb@gmail.com>
2021-05-15 20:33:59 +0530

    [MODIFIED] semester fetched in landing page

--- src/main/webapp/app/public/landing/landing.component.ts
+++ src/main/webapp/app/public/landing/landing.component.ts
@@ -1,6 +1,9 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ISemester } from 'app/entities/semester/semester.model';
 import { SemesterService } from 'app/entities/semester/service/semester.service';
+import { ISubject } from 'app/entities/subject/subject.model';
 
 @Component({
   selector: 'jhi-landing',
@@ -9,12 +12,16 @@ import { SemesterService } from 'app/entities/semester/service/semester.service'
 })
 export class LandingComponent implements OnInit {
   branchId?: number;
-
+  semesters?: ISemester[];
   constructor(private activatedRoute: ActivatedRoute, private semesterService: SemesterService) {}
 
   ngOnInit(): void {
     this.branchId = +this.activatedRoute.snapshot.paramMap.get('id')!;
     console.warn('init branch');
     console.warn(this.branchId);
+    this.semesterService.findByBranchId(this.branchId).subscribe((resp: HttpResponse<ISemester[]>) => {
+      this.semesters = resp.body ?? [];
+      console.warn(this.semesters);
+    });
   }
 }
